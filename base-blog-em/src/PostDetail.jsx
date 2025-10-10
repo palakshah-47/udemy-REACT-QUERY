2b610eb1f6c58f95bffb9e15e02412dc0820c4d8
palakshah-47 <palakshah47@gmail.com>
2025-10-10 09:20:16 -0400

    Post details change

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,21 +1,43 @@
-import { fetchComments } from "./api";
-import "./PostDetail.css";
+import { fetchComments } from './api';
+import './PostDetail.css';
+import { useQuery } from '@tanstack/react-query';
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+	const { id } = post;
+	const { data, isError, isLoading, error, isFetching } = useQuery({
+		queryKey: ['comments', id],
+		queryFn: () => fetchComments(id),
+		staleTime: 2000,
+	});
 
-  return (
-    <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
-      <p>{post.body}</p>
-      <h4>Comments</h4>
-      {data.map((comment) => (
-        <li key={comment.id}>
-          {comment.email}: {comment.body}
-        </li>
-      ))}
-    </>
-  );
+	if (isFetching) {
+		return <div>Fetching Post...</div>;
+	}
+
+	if (isLoading) {
+		return <div>Loading Post...</div>;
+	}
+	if (isError) {
+		return (
+			<>
+				<h3>Oops, Something went wrong</h3>
+				<p>Error: {error.toString()}</p>
+			</>
+		);
+	}
+
+	return (
+		<>
+			<h3 style={{ color: 'blue' }}>{post.title}</h3>
+			<button>Delete</button> <button>Update title</button>
+			<p>{post.body}</p>
+			<h4>Comments</h4>
+			{data.map((comment) => (
+				<li key={comment.id}>
+					{comment.email}: {comment.body}
+				</li>
+			))}
+		</>
+	);
 }
+
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,40 +1,58 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import { fetchPosts, deletePost, updatePost } from "./api";
-import { PostDetail } from "./PostDetail";
+import { fetchPosts, deletePost, updatePost } from './api';
+import { PostDetail } from './PostDetail';
+import { useQuery } from '@tanstack/react-query';
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
-  const [selectedPost, setSelectedPost] = useState(null);
+	const [currentPage, setCurrentPage] = useState(0);
+	const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+	const { data, isError, error, isLoading, isFetching } = useQuery({
+		queryKey: ['posts'],
+		queryFn: fetchPosts,
+		staleTime: 2000,
+	});
 
-  return (
-    <>
-      <ul>
-        {data.map((post) => (
-          <li
-            key={post.id}
-            className="post-title"
-            onClick={() => setSelectedPost(post)}
-          >
-            {post.title}
-          </li>
-        ))}
-      </ul>
-      <div className="pages">
-        <button disabled onClick={() => {}}>
-          Previous page
-        </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
-          Next page
-        </button>
-      </div>
-      <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
-    </>
-  );
+	if (isFetching) {
+		return <div>Fetching posts...</div>;
+	}
+
+	if (isLoading) {
+		return <div>Loading posts...</div>;
+	}
+
+	if (isError) {
+		return (
+			<>
+				<h3>Oops, Something went worng</h3>
+				<p></p>Error: {error.toString()}
+			</>
+		);
+	}
+
+	return (
+		<>
+			<ul>
+				{data.map((post) => (
+					<li key={post.id} className="post-title" onClick={() => setSelectedPost(post)}>
+						{post.title}
+					</li>
+				))}
+			</ul>
+			<div className="pages">
+				<button disabled onClick={() => {}}>
+					Previous page
+				</button>
+				<span>Page {currentPage + 1}</span>
+				<button disabled onClick={() => {}}>
+					Next page
+				</button>
+			</div>
+			<hr />
+			{selectedPost && <PostDetail post={selectedPost} />}
+		</>
+	);
 }
+
