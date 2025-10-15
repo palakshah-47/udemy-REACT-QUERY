0931617ee50f460f9cf3636a51868da357cb6856
palakshah-47 <palakshah47@gmail.com>
2025-10-15 17:16:07 -0400

    adding pagination

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,17 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import { fetchPosts, deletePost, updatePost } from './api';
 import { PostDetail } from './PostDetail';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { use } from 'react';
 const maxPostPage = 10;
 
 export function Posts() {
-	const [currentPage, setCurrentPage] = useState(0);
+	const [currentPage, setCurrentPage] = useState(1);
 	const [selectedPost, setSelectedPost] = useState(null);
 
+	const queryClient = useQueryClient();
+
+	useEffect(() => {
+		if( currentPage >= maxPostPage ) return;
+		const nextPage = currentPage + 1;
+		queryClient.prefetchQuery({
+			queryKey: ['posts', nextPage],
+			queryFn: () => fetchPosts(nextPage),
+		});
+	}, [currentPage, queryClient]);
+
 	const { data, isError, error, isLoading, isFetching } = useQuery({
-		queryKey: ['posts'],
-		queryFn: fetchPosts,
+		queryKey: ['posts', currentPage],
+		queryFn: () => fetchPosts(currentPage),
 		staleTime: 2000,
 	});
 
@@ -42,11 +54,21 @@ export function Posts() {
 				))}
 			</ul>
 			<div className="pages">
-				<button disabled onClick={() => {}}>
+				<button
+					disabled={currentPage <= 1}
+					onClick={() => {
+						setCurrentPage((page) => page - 1);
+					}}
+				>
 					Previous page
 				</button>
-				<span>Page {currentPage + 1}</span>
-				<button disabled onClick={() => {}}>
+				<span>Page {currentPage}</span>
+				<button
+					disabled={currentPage >= maxPostPage}
+					onClick={() => {
+						setCurrentPage((page) => page + 1);
+					}}
+				>
 					Next page
 				</button>
 			</div>
